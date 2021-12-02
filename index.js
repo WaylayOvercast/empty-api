f5b7bc5e3ae9fb388042d8478069d24c27cb9481
WaylayOvercast <timelyovercast@gmail.com>
2021-12-02 12:16:59 -0600

    yep

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 6060;
-server.use(express.json());
+
 
 
 server.get('/', (req, res) => {
